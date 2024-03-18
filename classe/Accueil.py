baf1b5f334a29825c5479baea2cf78c72f21afcf
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2024-03-18 15:47:21 +0100

    page d'accueil presque finie

---
+++ classe/Accueil.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+from PIL import Image, ImageTk
+
+class PageAccueil(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title("Démineur - Page d'Accueil")
+        self.geometry("800x600")
+
+        # Charger l'image et la redimensionner pour qu'elle corresponde à la taille de la fenêtre
+        self.background_image = Image.open("images/backround.jpg")
+        self.background_image = self.background_image.resize((800, 600), Image.LANCZOS)
+        self.background_image = ImageTk.PhotoImage(self.background_image)
+
+        # Créer un Canvas pour afficher l'image de fond
+        self.canvas = tk.Canvas(self, width=800, height=600)
+        self.canvas.pack(fill="both", expand=True)
+
+        # Afficher l'image de fond
+        self.canvas.create_image(0, 0, image=self.background_image, anchor="nw")
+
+        # Positionnement des boutons
+        y_coord = 0.45
+        button_spacing = 0.12
+
+        self.bouton_demarrer = tk.Button(self.canvas, text="Jouer", command=self.demarrer_jeu, font=("Times New Roman", 18, "bold"), fg="red", bg="black")
+        self.bouton_difficultes = tk.Button(self.canvas, text='Mode Difficultés', command=self.mode_difficultes, font=("Times New Roman", 18, "bold"), fg="red", bg="black")
+        self.bouton_quitter = tk.Button(self.canvas, text="Quitter", command=self.quitter_jeu, font=("Times New Roman", 18, "bold"), fg="red", bg="black")
+
+        self.bouton_demarrer.place(relx=0.5, rely=y_coord, anchor=tk.CENTER)
+        self.bouton_difficultes.place(relx=0.5, rely=y_coord + button_spacing, anchor=tk.CENTER)
+        self.bouton_quitter.place(relx=0.5, rely=y_coord + 2 * button_spacing, anchor=tk.CENTER)
+
+    def mode_difficultes(self):
+        print("Mode difficultés...")
+
+    def quitter_jeu(self):
+        self.destroy()
+
+    def demarrer_jeu(self):
+        print("Le jeu démarre...")
+
+if __name__ == "__main__":
+    page_accueil = PageAccueil()
+    page_accueil.mainloop()
