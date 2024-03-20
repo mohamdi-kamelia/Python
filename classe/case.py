db70ad07f71b5fdf57d8236d55471bc3e76e81c0
jean-renoul <jean.renoul@laplateforme.io>
2024-03-20 14:43:16 +0100

    case.py

--- classe/case.py
+++ classe/case.py
@@ -1,17 +1,7 @@
 class Case:
-    def __init__(self, revele=False, mine=False, minesAdjacentes=0, drapeau=False, interrogation=False):
+    def __init__(self, revele=False, mine=False, minesAdjacentes=0):
         self.revele = revele
         self.mine = mine
         self.minesAdjacentes = minesAdjacentes
-        self.drapeau = drapeau
-        self.interrogation = interrogation
-
-    def changerEtat(self):
-        if not self.revele:
-            if self.drapeau:
-                self.drapeau = False
-                self.interrogation = True
-            elif self.interrogation:
-                self.interrogation = False
-            else:
-                self.drapeau = True
+        
+    
