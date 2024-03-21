247db5f54bcc2137a9f979f368457295f5d921c4
mohamdikamelia <kamelia.mohamdi@laplateforme.io>
2024-03-21 12:07:27 +0100

    Commit des modifications locales dans Case.py

--- classe/Case.py
+++
@@ -1,7 +0,0 @@
-class Case:
-    def __init__(self, revele=False, mine=False, minesAdjacentes=0):
-        self.revele = revele
-        self.mine = mine
-        self.minesAdjacentes = minesAdjacentes
-        
-    
